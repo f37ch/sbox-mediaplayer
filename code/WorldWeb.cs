d961bdefafd68d430c54669fba5f16d4af268d61
f37ch <owltaver@gmail.com>
2023-07-09 22:16:05 +0300

    double worldPanel quality

--- code/WorldWeb.cs
+++ code/WorldWeb.cs
@@ -3,12 +3,12 @@ using Sandbox.UI;
 using System;
 
 namespace MediaPlayer;
-public partial class WorldWeb : WorldPanel
+public partial class WorldWeb:WorldPanel
 {
-	public WebPanel WebPanel { get; set; } 
-	public ScreenUI ScreenUI { get; set; }
-	public Controls Controls { get; set; }
-	public float PanelScale=0.7F,ScreenScale=1,Trigger=0;
+	public WebPanel WebPanel {get;set;} 
+	public ScreenUI ScreenUI {get;set;}
+	public Controls Controls {get;set;}
+	public float ScreenScale=1,Trigger=0;
 	private static float boundy,boundz;
 	public MPScreen Screen;
 	public WorldInput WorldInput=new();
@@ -17,13 +17,16 @@ public partial class WorldWeb : WorldPanel
 		Screen=scr;
 		WebPanel=AddChild<WebPanel>();
 		ScreenUI=WebPanel.AddChild<ScreenUI>();
+		//ScreenUI=WebPanel.AddChild<QueUI>();
 
 		boundy=Screen.Model.Bounds.Size.y-16;
-		boundz=Screen.Model.Bounds.Size.z-16;
+		boundz=Screen.Model.Bounds.Size.z-8;
+		
+		//PanelBounds = new Rect(-boundy/2,-boundz-8,boundy,boundz);//align with screen borders
+		//WorldScale=20;
 
-		PanelBounds = new Rect(-boundy/2,-boundz-8,boundy,boundz);//align with screen borders
-
-		WorldScale=20;
+		PanelBounds=new Rect(-boundy,-boundz,boundy*2,boundz*2-16);//align with screen borders
+		Transform=Transform.WithScale(10F);
 
 		Style.PointerEvents=PointerEvents.All;
 		WebPanel.Style.Width=Length.Percent(100);
@@ -37,18 +40,21 @@ public partial class WorldWeb : WorldPanel
 	{
 		if (Screen.Scale!=ScreenScale){
 			if (Game.IsClient){
-				PanelBounds = new Rect(-Screen.Scale*(boundy/2),-Screen.Scale*(boundz+8),Screen.Scale*boundy,Screen.Scale*boundz);
+				//PanelBounds = new Rect(-Screen.Scale*(boundy/2),-Screen.Scale*(boundz+8),Screen.Scale*boundy,Screen.Scale*boundz);
+				PanelBounds = new Rect(-Screen.Scale*boundy,-Screen.Scale*boundz,Screen.Scale*boundy*2,Screen.Scale*(boundz*2-16));
 				ScreenScale=Screen.Scale;
 			}
 		}
 		if (Game.IsClient){
 			if (Screen.IsValid){
 				//SceneObject.Bounds=Screen.WorldSpaceBounds;
+				//SceneObject.Transform.WithScale(.5F);
 				//DebugOverlay.Box(SceneObject.Bounds.Mins,SceneObject.Bounds.Maxs);
 				
 				Position=Screen.LocalPosition;
 				Rotation=Screen.LocalRotation;
-				Position=Position+Rotation.Backward*0.6F;
+				//Position=Position+Rotation.Backward*0.6F;
+				Position=Position+Rotation.Backward*0.6F+Rotation.Up*Screen.Scale*boundz/2;
 				Rotation=Rotation.LookAt(Rotation.Backward,Rotation.Up);
 
 				//var tr=Trace.Ray(Game.LocalPawn.AimRay.Position,Game.LocalPawn.AimRay.Position+Game.LocalPawn.AimRay.Forward*MaxInteractionDistance).Ignore(Game.LocalPawn).Run();
@@ -56,9 +62,6 @@ public partial class WorldWeb : WorldPanel
 			}else if(IsValid){
 				Delete();
 				WorldInput.Enabled=false;
-			}
-			if (Scale!=PanelScale){
-				Scale=PanelScale;
 			}
 			if (Controls.IsValid()){
 				if (Controls.Slider.IsValid()){
